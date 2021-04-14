04986858a5bcbb4a75cdf3687ffe631fcb56a73b
Jacob Cox <82506837+JCox06@users.noreply.github.com>
2021-04-14 11:17:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
   
  public static void main(String args[]) {
   Person person = new Person("james", 20);
-  System.out.println(person.getName())
+  System.out.println(person.getName());
  }
 }
