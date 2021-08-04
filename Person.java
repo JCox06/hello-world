57aa32270c581626f8f703de3164ede062c7cb7f
Jacob Cox <jacobcox2006@gmail.com>
2021-08-04 11:55:54 +0100

    Changed

--- Person.java
+++ Person.java
@@ -3,16 +3,3 @@ public class Person {
 private String name;
 private int age;
 
-public String getName() {
-return this.name;
-}
-  
-public int getAge() {
-return this.age;
-}
-  
-public Person(String name, int age) {
-this.name = name;
-this.age = age;
-}
-}
